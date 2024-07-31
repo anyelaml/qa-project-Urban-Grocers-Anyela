f7fa01f844f268515fb57e3a93639a0c8acb3aa0
Anyela <anyela.malaga@ucsp.edu.pe>
2024-07-30 23:13:02 -0500

    Prueba1

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-82cde57f-a93e-46cb-9c73-ac9a26d65383.containerhub.tripleten-services.com"  # Inserta tu dirección de URL sin la barra diagonal al final
+URL_SERVICE = "https://cnt-0e46f39b-c5ca-4db6-9ff0-968de9ea8fa6.containerhub.tripleten-services.com"  # Inserta tu dirección de URL sin la barra diagonal al final
 CREATE_USER_PATH = "/api/v1/users"  # Almacena la ruta para crear un usuario o usuaria en esta variable
 KITS_PATH = "/api/v1/kits"  # Almacena la ruta para crear un kit en esta variable
